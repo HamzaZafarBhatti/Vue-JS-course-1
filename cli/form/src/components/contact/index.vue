d91cdb83204c69d5a6fc775f4df24f073773b025
HamzaZafarBhatti <hamza0952454@gmail.com>
2022-11-11 18:21:08 +0500

    vadv

--- cli/form/src/components/contact/index.vue
+++ cli/form/src/components/contact/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <form @submit.prevent="submitForm">
+  <form @submit.prevent="checkForm">
     <div class="row">
       <div class="col-xl-12">
         <h1>Contact us</h1>
@@ -68,7 +68,7 @@
               id="promotions"
               v-model="formData.spam"
             />
-            <label class="form-check-label" for="newsletter">
+            <label class="form-check-label" for="promotions">
               Promotions
             </label>
           </div>
@@ -109,6 +109,12 @@
         </div>
 
         <button class="btn btn-primary">Submit</button>
+        <hr>
+        <p v-if="errors.length">
+          <ul>
+            <li v-for="error in errors" :key="error">{{ error  }}</li>
+          </ul>
+        </p>
       </div>
     </div>
   </form>
@@ -118,6 +124,7 @@
 export default {
   data() {
     return {
+      errors: [],
       countries: [
         "Pakistan",
         "India",
@@ -130,10 +137,10 @@ export default {
         "Japan",
       ],
       formData: {
-        name: "hamza",
-        email: "hamza@gmail.com",
-        subject: "blah blah",
-        message: "bsdfb",
+        name: "",
+        email: "",
+        subject: "",
+        message: "",
         // One way to handle checkboxes
         // newsletter: false,
         // promotions: false,
@@ -148,6 +155,25 @@ export default {
     submitForm() {
       console.log(this.formData);
     },
+    checkForm() {
+      this.errors = []
+      if(!this.formData.name) {
+        this.errors.push('Name is required!')
+      }
+      if(!this.formData.email) {
+        this.errors.push('Email is required!')
+      } else if(!this.validEmail(this.email)) {
+        this.errors.push('Email is not valid!')
+      }
+
+      if(!this.errors.length) {
+        this.submitForm()
+      }
+    },
+    validEmail(email) {
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      return re.test(email)
+    }
   },
 };
 </script>
