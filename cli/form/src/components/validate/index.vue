ad631523d3e3a497c1e4327e9a2627f79e8cbd64
HamzaZafarBhatti <hamza0952454@gmail.com>
2022-11-30 18:26:59 +0500

    sav

--- cli/form/src/components/validate/index.vue
+++ cli/form/src/components/validate/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Form>
+    <Form @submit="onSubmit">
       <div class="form-group">
         <label for="name">Name</label>
         <Field
@@ -17,21 +17,28 @@
       </ErrorMessage>
       <div class="form-group">
         <label for="email">Email</label>
-        <Field name="email" :rules="validateEmail" v-slot="{ field, errors }">
+        <Field name="email" :rules="validateEmail" v-slot="{ field, errors, errorMessage }">
           <input
             type="text"
             name="email"
             class="form-control"
             v-bind="field"
-            :class="{ 'is-invalid': errors.length != 0 }"
+            :class="{ 'is-invalid': errors.length !== 0 }"
           />
+          <div class="alert alert-danger" role="alert" v-if="errors.length !== 0">
+            {{ errorMessage }}
+          </div>
         </Field>
       </div>
-      <ErrorMessage name="email" as="div" v-slot="{ message }">
-        <div class="alert alert-danger" role="alert">
-          {{ message }}
-        </div>
-      </ErrorMessage>
+      <div class="form-group">
+        <label for="message">Message</label>
+        <Field name="message" :rules="validateMessage" v-slot="{ field, errors, errorMessage }">
+          <textarea name="message" cols="2" rows="5" class="form-control" :class="{ 'is-invalid': errors.length !== 0 }" id="message" v-bind="field"></textarea>
+          <div class="alert alert-danger" role="alert" v-if="errors.length !== 0">
+            {{ errorMessage }}
+          </div>
+        </Field>
+      </div>
       <hr />
       <button class="btn btn-primary">Submit</button>
     </Form>
@@ -69,11 +76,24 @@ export default {
       }
       return true;
     },
+    validateMessage(value) {
+      if (!value) {
+        return "Message field is required"
+      }
+      // if (!this.validEmail(value)) {
+      //   return "Bad Email";
+      // }
+      return true;
+    },
     validEmail(email) {
       const re =
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       return re.test(email);
     },
+    onSubmit(values, {resetForm}) {
+      console.log(values)
+      resetForm()
+    }
   },
 };
 </script>
