d1fd3f095d0d3856b6d7f4cbc261171866cb174c
HamzaZafarBhatti <hamza0952454@gmail.com>
2023-01-11 17:53:31 +0500

    router half done

--- cli/routes/src/components/header_footer/Header.vue
+++ cli/routes/src/components/header_footer/Header.vue
@@ -1,14 +1,31 @@
 <template>
-<div class="container">
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+  <div class="container">
+    <header
+      class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom"
+    >
+      <a
+        href="/"
+        class="
+          d-flex
+          align-items-center
+          mb-3 mb-md-0
+          me-md-auto
+          text-dark text-decoration-none
+        "
+      >
         <span class="fs-4">Beardit.com</span>
       </a>
 
       <ul class="nav nav-pills">
-        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Articles</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+        <li class="nav-item">
+          <router-link to="/" class="nav-link">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/articles" class="nav-link">Articles</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/contact" class="nav-link">Contact</router-link>
+        </li>
       </ul>
     </header>
   </div>
@@ -16,11 +33,9 @@
 
 
 <style>
-    body {
-       padding:0;
-       margin:0;
-       font-family: 'Roboto', sans-serif;
-   }
-
-
+body {
+  padding: 0;
+  margin: 0;
+  font-family: "Roboto", sans-serif;
+}
 </style>
--- cli/routes/src/main.js
+++ cli/routes/src/main.js
@@ -1,11 +1,29 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import Header from './components/header_footer/Header';
 import Footer from './components/header_footer/Footer';
+import Articles from "./components/Articles";
+import Contact from "./components/Contact";
+import Home from "./components";
+import Article from "./components/Articles/article";
 
 const app =  createApp(App);
 
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path: '/', component: Home },
+        {path: '/articles', component: Articles },
+        {path: '/article/:id', component: Article },
+        {path: '/contact', component: Contact },
+    ],
+    linkActiveClass: 'active'
+})
+
 app.component('app-header',Header);
 app.component('app-footer',Footer);
+
+app.use(router)
 app.mount('#app')
