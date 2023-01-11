8dc6f07cb51c599099fc48a4ec1926f34b9c57d4
HamzaZafarBhatti <hamza0952454@gmail.com>
2023-01-11 12:28:03 +0500

    request demo

---
+++ cli/request-demo/src/components/Users.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="row">
+    <div class="col-auto mb-4" v-for="(user) in users" :key="user.id">
+      <div class="card" style="width: 14rem">
+        <img
+          class="card-img-top"
+          :src="'https://placebeard.it/300/300?' + user.id"
+          alt="Card image cap"
+        />
+        <div class="card-body">
+          <div class="card-text"><strong>Name:</strong> {{ user.name }}</div>
+          <div class="card-text"><strong>Lastname:</strong> {{ user.lastname }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import axios from "axios";
+import loader from "Utils/loader";
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  mounted() {
+    this.loadUsers();
+  },
+  methods: {
+    loadUsers() {
+      axios
+        .get("http://localhost:3004/users")
+        .then((response) => {
+          this.users = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
